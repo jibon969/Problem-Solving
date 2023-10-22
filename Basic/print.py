c441bf5862e2ecc5741ca6631ac80cc9844ddf06
jibon969 <jayed.swe@gmail.com>
2023-10-22 22:50:38 +0600

    change number

--- Basic/print.py
+++ Basic/print.py
@@ -15,7 +15,13 @@ for i in range(1, n+1):
 
 # print_number(3)
 
-n = int(input())
-for i in range(1, n+1):
-    print(i, end="")
-        
+# n = int(input())
+# for i in range(1, n+1):
+#     print(i, end="")
+
+# Function
+def check_number(n):
+    for i in range(1, n+1):
+        print(i, end="")
+
+check_number(4)

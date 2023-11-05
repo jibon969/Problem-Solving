7abd0eb2e3d9367fe4216ab94111c2fe7a1f239f
jibon969 <jayed.swe@gmail.com>
2023-11-05 20:05:44 +0600

    added area of the triangle

---
+++ Programiz/04_area_of_a_triangle.py
@@ -0,0 +1,8 @@
+# Calculate the Area of a Triangle
+# Triangle Formula = A = ½ × Base × Height (Perpendicular distance)
+
+base = float(input("Enter the base length of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+area = 0.5 * base * height
+print("The area of the triangle is : ", area)

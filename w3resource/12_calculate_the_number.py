fbe4c32f3b8274f3aa44f2835f197fd7a7eae540
jibon969 <jayed.swe@gmail.com>
2023-11-11 20:34:56 +0600

    added date time

--- w3resource/12_calculate_the_number.py
+++ w3resource/12_calculate_the_number.py
@@ -1,2 +1,8 @@
 # Write a Python program to calculate the number of days between two dates.
 # Sample dates : (2014, 7, 2), (2014, 7, 11)
+
+from datetime import date
+x = date(2014, 7, 2)
+y = date(2014, 7, 11)
+result = x - y
+print(result)

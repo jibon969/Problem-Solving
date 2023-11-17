ee3fc6e4bc1eb0e2460c07b3ead57c6dbe03127b
jibon969 <jayed.swe@gmail.com>
2023-11-17 23:48:13 +0600

    added program sum

---
+++ w3resource/19_program.py
@@ -0,0 +1,11 @@
+# Write a Python program to solve (x + y) * (x + y).
+
+"""
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
+"""
+
+x = 4, y = 3
+
+output =  x * x + 2 * x * y + y * y
+print(output)

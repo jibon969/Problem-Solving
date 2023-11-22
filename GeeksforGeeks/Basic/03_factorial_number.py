4298c0a66b00fb6ec18bb321642e6809f3498b0b
jibon969 <jayed.swe@gmail.com>
2023-11-22 23:54:53 +0600

    addd function in factorial number

--- GeeksforGeeks/Basic/03_factorial_number.py
+++ GeeksforGeeks/Basic/03_factorial_number.py
@@ -5,3 +5,15 @@ number = 1
 for i in range(1, factorial_number+1):
     number = number * i
 print(number)
+
+
+# Using function ===========================
+def factorial_number(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    
+        return f
+
+result = factorial_number(5)
+print(result)

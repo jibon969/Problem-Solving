d58952faeb0ed8f05f63aad1943dc7b9cee64736
jibon969 <jayed.swe@gmail.com>
2023-11-24 23:24:32 +0600

    added prime_numbers_interval

---
+++ GeeksforGeeks/Basic/08_prime_numbers_interval.py
@@ -0,0 +1,11 @@
+# Print all Prime numbers in an Interval
+
+start_num = int(input("Enter start number : "))
+end_num = int(input("Enter end number : "))
+
+for num in range(start_num, end_num+1):
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)

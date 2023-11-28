78f9b5c0f8193adcd884c8245fc3757a2d6316dd
jibon969 <jayed.swe@gmail.com>
2023-11-28 23:24:15 +0600

    user input from fibonacci_number

--- GeeksforGeeks/Basic/10_fibonacci_number.py
+++ GeeksforGeeks/Basic/10_fibonacci_number.py
@@ -3,8 +3,8 @@
 def fibonacci_number(n):
         a = 0
         b = 1
-        # print(a)
-        # print(b)
+        print(a)
+        print(b)
 
         for i in range(2, n):
                 c = a + b
@@ -12,4 +12,4 @@ def fibonacci_number(n):
                 b = c
                 print(c)
 
-fibonacci_number(10)
+fibonacci_number(int(input("Enter your number : ")))

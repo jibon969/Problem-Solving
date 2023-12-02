0812d3a76c0979173507f499318e3622496f2455
jibon969 <jayed.swe@gmail.com>
2023-12-02 23:37:11 +0600

    added isFibonacci function

--- GeeksforGeeks/Basic/11.check_fibonacci_number.py
+++ GeeksforGeeks/Basic/11.check_fibonacci_number.py
@@ -1,11 +1,28 @@
 # How to check if a given number is Fibonacci number?
 
+# n = int(input("Enter your number : "))
 
-a = 0
-b = 1
+# a = 0
+# b = 1
 
-for i in range(10):
-        i = a + b
-        a = b
-        b = i
-        print(i)
+# for i in range(n):
+#         i = a + b
+#         a = b
+#         b = i
+#         print(i)
+    
+
+def isFibonacci(num):
+        fib1 = 0
+        fib2 = 1
+        sum =  fib1 + fib2
+
+        while (sum <= num):
+                if num == sum:
+                        return True
+        return False
+
+output = isFibonacci(10)
+print(output)
+        
+      

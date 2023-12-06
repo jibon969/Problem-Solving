3771b133b4123a6130cbd467affeea5f2440f64b
jibon969 <jayed.swe@gmail.com>
2023-12-06 21:50:02 +0600

    added Find largest element

--- GeeksforGeeks/Array/02_find_largest_element_array.py
+++ GeeksforGeeks/Array/02_find_largest_element_array.py
@@ -1,1 +1,13 @@
 # Find largest element in an array
+
+def large_element(arr, n):
+        max = arr[0]
+        for i in range(1, n):
+                if arr[i] > max:
+                        max = arr[i]
+        return max
+
+arr = [10, 20, 30]
+n = len(arr)
+output = large_element(arr, n)
+print(output)

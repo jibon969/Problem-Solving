6f74e27397fb9b955e0898c7dcfe43f90d284b62
jibon969 <jayed.swe@gmail.com>
2024-04-29 23:19:46 +0600

    added build in function

--- w3resource/09_built_in_function.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
-Sample function : abs()
-Expected Result :
-"""
-
-number =  5
-output = abs(number)
-print(output)

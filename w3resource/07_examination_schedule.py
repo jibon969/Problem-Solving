e2b6a87258cf94f42d988edee39a6437fc5130c1
jibon969 <jayed.swe@gmail.com>
2024-05-01 22:59:41 +0600

    remove sme

--- w3resource/07_examination_schedule.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to display the examination schedule. (extract the date from exam_st_date).
-# 11 / 12 / 2014
-
-from datetime import datetime
-now = datetime.now()
-
-date_time = now.strftime("%m/%d/%Y")
-print(date_time)

eec076d33dc8067489da82e57dfdf6e3ed29a976
jibon969 <jayed.swe@gmail.com>
2024-05-06 22:41:58 +0600

    added random number

--- Programiz/05_swap_two_variables.py
+++ Programiz/05_swap_two_variables.py
@@ -7,10 +7,20 @@
 # print(b)
 
 # Another Example
+# a = 5
+# b = 6
+# a = a + b
+# b = a - b
+# a = a - b
+# print(a)
+# print(b)
+
+
+# Another Example
 a = 5
 b = 6
-a = a + b
-b = a - b
-a = a - b
+temp = a
+a = b
+b = temp
 print(a)
 print(b)

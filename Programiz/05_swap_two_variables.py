79cba790f02f2218bdc97650551c1bb4c82433fd
jibon969 <jayed.swe@gmail.com>
2024-05-06 22:25:05 +0600

    added swap variable

--- Programiz/05_swap_two_variables.py
+++ Programiz/05_swap_two_variables.py
@@ -1,10 +1,16 @@
 # Swap Two Variables
 
-a = 1
-b = 2
-a,b = b, a
+# a = 1
+# b = 2
+# a,b = b, a
+# print(a)
+# print(b)
+
+# Another Example
+a = 5
+b = 6
+a = a + b
+b = a - b
+a = a - b
 print(a)
 print(b)
-
-
-

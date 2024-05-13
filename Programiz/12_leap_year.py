f7f55a915a55cd7ad639a99bbf7b7cdb081918ed
jibon969 <jayed.swe@gmail.com>
2024-05-13 21:10:47 +0600

    added find large number

--- Programiz/12_leap_year.py
+++ Programiz/12_leap_year.py
@@ -6,4 +6,4 @@ year = 2000
 if year % 100 == 0 and year % 400 == 0:
     print("Leap Year")
 else:
-    print("Not Leap year !")
+    print("Not Leap year !")  
---
+++ Programiz/13_largest_three_numbers.py
@@ -0,0 +1,26 @@
+# Find the Largest Among Three Numbers
+
+a = 619
+b = 198
+c = 99
+
+if a > b and a > c:
+    print("A is Large number")
+elif b > a and b > c:
+    print("B is Large number")
+else:
+    print("C is Large number")
+
+
+# More Example
+num1 = 6
+num2 = 9
+num3 = 2
+if num1 > num2 and num1 > num3:
+    print("num1 is big")
+elif num2 > num3:
+    print("num2 is big")
+else:
+    print("num3 is big")
+
+

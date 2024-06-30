16aa4e764e832bbc01c1db9cad253b85e2d156bb
jibon969 <jayed.swe@gmail.com>
2024-06-30 22:22:02 +0600

    added area of a circle

---
+++ geeksforgeeks/07_area_circle.py
@@ -0,0 +1,15 @@
+
+# Find area of a circle
+
+"""
+Input: 5
+Output : 78.55
+Formula: Area = pi * r2
+where r is radius of circle
+"""
+
+def find_area_circle(area):
+        pi = 3.142
+        return pi * (area*area)
+result = find_area_circle(5)
+print(result)

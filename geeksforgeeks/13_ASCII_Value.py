e05e7755e083b1f56240c0b147e0c5482f514c94
jibon969 <jayed.swe@gmail.com>
2024-07-09 21:45:13 +0600

    added ASCII_Value

---
+++ geeksforgeeks/13_ASCII_Value.py
@@ -0,0 +1,15 @@
+# ASCII Value 
+"""
+Input : a 
+Output : 97
+"""
+
+"""
+Python ord() function returns the Unicode code from a given character. 
+It converts the given string of length one, 
+returns an integer representing the Unicode code point of the character. 
+"""
+
+x = 'a'
+result = ord(x)
+print(result)

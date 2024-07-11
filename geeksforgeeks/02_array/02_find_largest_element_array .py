c48b187732c38ec01ed6bbbccddcf7c1b72bf3cf
jibon969 <jayed.swe@gmail.com>
2024-07-11 23:24:16 +0600

    added find large number

--- geeksforgeeks/02_array/02_find_largest_element_array .py
+++ geeksforgeeks/02_array/02_find_largest_element_array .py
@@ -1,1 +1,12 @@
 # Find Largest Element in an Array
+
+"""
+Input : arr[] = {10, 20, 4}
+Output : 20
+Input : arr[] = {20, 10, 20, 4, 100}
+Output : 100
+"""
+import math
+my_list = [10, 20, 4]
+for i in my_list:
+    print(i)
